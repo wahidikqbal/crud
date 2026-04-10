462fe1964e0973912b9ab9080dae9c72f302467a
Iqbal Digital <wahidikqbal@gmail.com>
2026-04-10 10:04:41 +0700

    menambahakan Tag dan Seeder

---
+++ lib/crud_web/live/tag_live/show.ex
@@ -0,0 +1,37 @@
+defmodule CrudWeb.TagLive.Show do
+  use CrudWeb, :live_view
+
+  alias Crud.Blog
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash}>
+      <.header>
+        Tag {@tag.id}
+        <:subtitle>This is a tag record from your database.</:subtitle>
+        <:actions>
+          <.button navigate={~p"/tags"}>
+            <.icon name="hero-arrow-left" />
+          </.button>
+          <.button variant="primary" navigate={~p"/tags/#{@tag}/edit?return_to=show"}>
+            <.icon name="hero-pencil-square" /> Edit tag
+          </.button>
+        </:actions>
+      </.header>
+
+      <.list>
+        <:item title="Name">{@tag.name}</:item>
+      </.list>
+    </Layouts.app>
+    """
+  end
+
+  @impl true
+  def mount(%{"id" => id}, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:page_title, "Show Tag")
+     |> assign(:tag, Blog.get_tag!(id))}
+  end
+end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,38 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+import Ecto.Query
+alias Crud.Repo
+alias Crud.Blog
+
+# Hapus data existing untuk menghindari duplikat saat menjalankan ulang seeder
+Repo.delete_all(from c in Blog.Category, where: c.id > 0)
+Repo.delete_all(from t in Blog.Tag, where: t.id > 0)
+
+# ============ SEED CATEGORIES ============
+category_names = [
+  "category 1",
+  "category 2",
+  "category 3",
+  "category 4",
+  "category 5"
+]
+
+for name <- category_names do
+  Blog.create_category(%{name: name})
+end
+
+# ============ SEED TAGS ============
+tag_names = [
+  "tag 1",
+  "tag 2",
+  "tag 3",
+  "tag 4",
+  "tag 5"
+]
+
+for name <- tag_names do
+  Blog.create_tag(%{name: name})
+end
+
+IO.puts("Seeded #{length(category_names)} categories and #{length(tag_names)} tags!")
